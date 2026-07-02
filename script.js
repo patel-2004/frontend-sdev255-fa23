be550af002ac743d77dfd52c2449febfb366e9fb
Suhani <Spatel158@ivytech.edu>
2026-07-03 03:19:13 +0530

    connected frontend to backend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //we are going to make an event listener.. it will trigger with the DOM is loaded (aka upon visiting webpage)
 addEventListener("DOMContentLoaded", async function(){
-    const response = await fetch("http://localhost:3000/api/songs")
+    const response = await fetch("https://backend-255-fa23-r4wc.onrender.com/api/songs")
     const songs = await response.json()
 
     let html = ""
